dee44d7556cf83b513805c0dd32e55377cdee579
JOJI <93754303+J0J1-ALT@users.noreply.github.com>
2025-02-01 12:18:51 -0500

    Update firebase.ts

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, doc, setDoc, getDoc, deleteDoc, getDocs, QueryDocumentSnapshot } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUal37aYkfMmb75rVArvFKPv4ktGx0WOc",
+  apiKey: "AIzaSyXXXXXXXEXAMPLEKEY",
   authDomain: "prueba-tecnica-89fa8.firebaseapp.com",
   projectId: "prueba-tecnica-89fa8",
   storageBucket: "prueba-tecnica-89fa8.firebasestorage.app",
@@ -10,6 +10,7 @@ const firebaseConfig = {
   appId: "1:1003616981151:web:9f1bcb55c9acd0d52e12aa",
   measurementId: "G-4VRJP1DYLE"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
